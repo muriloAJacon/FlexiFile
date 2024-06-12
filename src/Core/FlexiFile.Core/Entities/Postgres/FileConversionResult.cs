7745da30f5f6ca5fe4780760eccc08560db63e5f
Murilo Azevedo Jacon <murilo.ajac@gmail.com>
2024-06-11 23:22:03 -0300

    Fixed tabs and scapes mix

--- src/Core/FlexiFile.Core/Entities/Postgres/FileConversionResult.cs
+++ src/Core/FlexiFile.Core/Entities/Postgres/FileConversionResult.cs
@@ -10,29 +10,29 @@ namespace FlexiFile.Core.Entities.Postgres;
 [Index("FileConversionId", Name = "IX_FileConversionResult_file_conversion_id")]
 public partial class FileConversionResult
 {
-    [Key]
-    [Column("id")]
-    public Guid Id { get; set; }
+	[Key]
+	[Column("id")]
+	public Guid Id { get; set; }
 
 	[Column("type_id")]
 	public int TypeId { get; set; }
 
 	[Column("file_conversion_id")]
-    public Guid FileConversionId { get; set; }
+	public Guid FileConversionId { get; set; }
 
-    [Column("creation_date")]
-    [Precision(3, 0)]
-    public DateTime CreationDate { get; set; }
+	[Column("creation_date")]
+	[Precision(3, 0)]
+	public DateTime CreationDate { get; set; }
 
 	[Column("size")]
 	public long Size { get; set; }
 
 	[Column("order")]
-    public int Order { get; set; }
+	public int Order { get; set; }
 
-    [ForeignKey("FileConversionId")]
-    [InverseProperty("FileConversionResults")]
-    public virtual FileConversion FileConversion { get; set; } = null!;
+	[ForeignKey("FileConversionId")]
+	[InverseProperty("FileConversionResults")]
+	public virtual FileConversion FileConversion { get; set; } = null!;
 
 	[ForeignKey("TypeId")]
 	[InverseProperty("FileConversionResults")]
